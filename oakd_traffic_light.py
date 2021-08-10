96e64fd172c48981b9f898cb084a1aa8848f0ca2
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2021-08-10 12:58:58 +0800

    Update oakd_traffic_light.py

--- oakd_traffic_light.py
+++ oakd_traffic_light.py
@@ -2,11 +2,15 @@
 """
 Created on Sat Jul 17 01:31:31 2021
 
-@author: e-default
+@author: Wong Yi Jie
 """
 from depthai_utils import TrafficPipeline
 
 modelPath = r'models/trafficlight_frozen_darknet_yolov4_model_openvino_2021.3_5shave.blob'
 label = ['red', 'green']
 trafficPipeline = TrafficPipeline(modelPath, label)
-trafficPipeline.run()
+
+try:
+    trafficPipeline.run()
+except:
+    raise
